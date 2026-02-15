b9cd174635787ab5f1420e4c2ac91d31f8e41d83
unknown <micr647@gmail.com>
2026-02-15 22:46:47 +0600

    Prepare Django project for deployment

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-il+!h8tvnq2cue%4u#rn@b4%0bm+q$2w4n_$dj&o@w7!zat!b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
